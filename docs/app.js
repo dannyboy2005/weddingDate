837db3b49a45c6fba7bf7a8597772c3e3afd6ae8
Daniel Boffey <dboffey70@googlemail.com>
2020-03-20 14:51:08 +0000

    changes

--- docs/app.js
+++ docs/app.js
@@ -1,7 +1,31 @@
 // Show additional guest meals
-const attendee = document.getElementById("attendees").value;
+let invitees;
+const attenders = document
+  .getElementById("attendees")
+  .addEventListener("change", function() {
+    invitees = this.value;
+  });
+const guests = document
+  .getElementById("attendees")
+  .addEventListener("change", checkAttendees(invitees));
 
-if (attendee === 2) {
-  const element = document.getElementById("starter-guest-2");
-  element.classList.toggle("display-none");
+function checkAttendees(n) {
+  if (n.value === 1) {
+  } else if (n.value === 2) {
+    document.getElementById("starter-guest-2").classList.toggle("display-none");
+  } else if (n.value === 3) {
+  } else {
+  }
 }
+
+// document.getElementById("attendees").addEventListener("change", function()
+
+// if (this.value === 2) {
+//   document
+//     .getElementById("starter-guest-2")
+//     .classList.remove("display-none");
+// }
+
+// if (guests === 2) {
+//   const element = document.getElementById("starter-guest-2");
+//   element.classList.toggle("display-none");
