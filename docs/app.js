11dcbbf0d0ef99e27b800643cf03b56c8490ec5b
Daniel Boffey <dboffey70@googlemail.com>
2020-03-23 11:11:19 +0000

    completed 1.0

--- docs/app.js
+++ docs/app.js
@@ -1,31 +1,63 @@
-// Show additional guest meals
-let invitees;
-const attenders = document
-  .getElementById("attendees")
-  .addEventListener("change", function() {
-    invitees = this.value;
-  });
-const guests = document
-  .getElementById("attendees")
-  .addEventListener("change", checkAttendees(invitees));
+// // var attendees = 1;
+// const starterGuest2 = document.getElementById("starter-guest-2");
 
-function checkAttendees(n) {
-  if (n.value === 1) {
-  } else if (n.value === 2) {
-    document.getElementById("starter-guest-2").classList.toggle("display-none");
-  } else if (n.value === 3) {
-  } else {
-  }
-}
-
-// document.getElementById("attendees").addEventListener("change", function()
-
-// if (this.value === 2) {
-//   document
-//     .getElementById("starter-guest-2")
-//     .classList.remove("display-none");
+// function toggle() {
+//   // attendees = document.getElementById("attendees").value;
+//   if (this.value === 2) {
+//     starterGuest2.classList.toggle("display-none");
+//   }
 // }
 
-// if (guests === 2) {
-//   const element = document.getElementById("starter-guest-2");
-//   element.classList.toggle("display-none");
+$("#attendees").on("change", function() {
+  var optionSelected = $("option:selected", this);
+  var valueSelected = this.value;
+  if (valueSelected == 2) {
+    $("#starter-guest-2").show();
+    $("#main-guest-2").show();
+    $("#sides-guest-2").show();
+
+    $("#starter-guest-3").hide();
+    $("#main-guest-3").hide();
+    $("#sides-guest-3").hide();
+
+    $("#starter-guest-4").hide();
+    $("#main-guest-4").hide();
+    $("#sides-guest-4").hide();
+  } else if (valueSelected == 3) {
+    $("#starter-guest-2").show();
+    $("#main-guest-2").show();
+    $("#sides-guest-2").show();
+
+    $("#starter-guest-3").show();
+    $("#main-guest-3").show();
+    $("#sides-guest-3").show();
+
+    $("#starter-guest-4").hide();
+    $("#main-guest-4").hide();
+    $("#sides-guest-4").hide();
+  } else if (valueSelected == 4) {
+    $("#starter-guest-2").show();
+    $("#main-guest-2").show();
+    $("#sides-guest-2").show();
+
+    $("#starter-guest-3").show();
+    $("#main-guest-3").show();
+    $("#sides-guest-3").show();
+
+    $("#starter-guest-4").show();
+    $("#main-guest-4").show();
+    $("#sides-guest-4").show();
+  } else {
+    $("#starter-guest-2").hide();
+    $("#main-guest-2").hide();
+    $("#sides-guest-2").hide();
+
+    $("#starter-guest-3").hide();
+    $("#main-guest-3").hide();
+    $("#sides-guest-3").hide();
+
+    $("#starter-guest-4").hide();
+    $("#main-guest-4").hide();
+    $("#sides-guest-4").hide();
+  }
+});
